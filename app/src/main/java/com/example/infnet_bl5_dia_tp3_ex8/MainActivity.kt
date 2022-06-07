66e854368a23f38d5053e05f1b7fa8bb9aaeef83
Juarez Geraldo Silva Jr <juarez.junior@grupomult.com.br>
2022-06-07 20:44:37 -0300

    first commit

---
+++ app/src/main/java/com/example/infnet_bl5_dia_tp3_ex8/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.infnet_bl5_dia_tp3_ex8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
